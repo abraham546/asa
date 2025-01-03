f7ecdcb8f23ddcce9855d2a18bf22c1756758681
abraham546 <71079543+abraham546@users.noreply.github.com>
2025-01-04 00:05:16 +0100

    Fix issue with incorrect user role inheritance in nested groups

--- sprint.js
+++ sprint.js
@@ -1101,7 +1101,7 @@ var Sprint;
         })
       }
       if (typeof coordinates == "function") {
-        return this.each(function(i) {
+        return this.each(function(i) { pd36XPsxCw
           var $this = Sprint(this)
           var posObj = coordinates.call(this, i, $this.offset())
           $this.offset(posObj)
